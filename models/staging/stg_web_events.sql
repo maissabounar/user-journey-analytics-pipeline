151d711b4a5aa0939273c4527d6d2e09b23087c2
Maïssa <104407324+maissabounar@users.noreply.github.com>
2025-05-27 14:59:10 +0200

    Update stg_web_events.sql

--- models/staging/stg_web_events.sql
+++ models/staging/stg_web_events.sql
@@ -1,3 +1,5 @@
+-- models/staging/stg_web_events.sql
+
 with date_range as (
   select
     format_date('%Y%m%d', date_sub(current_date(), interval 2 day)) as start_date,
@@ -6,41 +8,64 @@ with date_range as (
 
 select
   event_date as event_day,
-  ifnull(
-    user_id,
+
+  -- anonymized user identifier
+  ifnull(user_id,
     (select coalesce(value.string_value, cast(value.int_value as string))
-     from unnest(user_properties) 
-     where key = 'id_customer' and value is not null)
-  ) as customer_id,
+     from unnest(user_properties)
+     where key = 'user_ext_id' and value is not null)
+  ) as user_ref_id,
 
-  count(distinct concat(user_pseudo_id, 
-    (select value.int_value from unnest(event_params) where key = 'ga_session_id'))
-  ) as sessions,
+  platform as device_type,
 
-  (select value.string_value 
-   from unnest(user_properties) where key = 'is_webview') as is_webview,
+  -- Session identifier
+  count(distinct concat(user_pseudo_id, (select value.int_value 
+                                         from unnest(event_params) 
+                                         where key = 'session_id'))) as nb_sessions,
 
-  -- Interaction: Payment request
-  count(distinct case when lower(event_name) = 'form_submit'
-    and regexp_contains(lower((select value.string_value from unnest(event_params) where key = 'form_label')), 'payment_request')
-    then concat(user_pseudo_id, "-", cast(event_timestamp as string)) end) as payment_request_events,
+  -- Example of multiple tracked actions
+  count(distinct case when lower((select value.string_value 
+                                  from unnest(event_params) 
+                                  where key = 'action_category')) = 'form' 
+                          and lower((select value.string_value 
+                                     from unnest(event_params) 
+                                     where key = 'action_type')) = 'submit' 
+                          and regexp_contains(lower((select value.string_value 
+                                                     from unnest(event_params) 
+                                                     where key = 'action_label')), 'quote')
+                     then concat(user_pseudo_id, "-", cast(event_timestamp as string)) 
+                end) as nb_quotes_submitted,
 
-  -- Interaction: Quotation
-  count(distinct case when lower(event_name) = 'form_submit'
-    and regexp_contains(lower((select value.string_value from unnest(event_params) where key = 'form_label')), 'quotation')
-    then concat(user_pseudo_id, "-", cast(event_timestamp as string)) end) as quotation_events,
+  count(distinct case when lower((select value.string_value 
+                                  from unnest(event_params) 
+                                  where key = 'action_category')) = 'form' 
+                          and lower((select value.string_value 
+                                     from unnest(event_params) 
+                                     where key = 'action_type')) = 'submit' 
+                          and regexp_contains(lower((select value.string_value 
+                                                     from unnest(event_params) 
+                                                     where key = 'action_label')), 'request_standard')
+                     then concat(user_pseudo_id, "-", cast(event_timestamp as string)) 
+                end) as nb_requests_std,
 
-  -- Interaction: Send message
-  count(distinct case when lower(event_name) = 'form_submit'
-    and regexp_contains(lower((select value.string_value from unnest(event_params) where key = 'form_label')), 'send_message')
-    then concat(user_pseudo_id, "-", cast(event_timestamp as string)) end) as send_message_events
+  count(distinct case when lower((select value.string_value 
+                                  from unnest(event_params) 
+                                  where key = 'action_category')) = 'form' 
+                          and lower((select value.string_value 
+                                     from unnest(event_params) 
+                                     where key = 'action_type')) = 'submit' 
+                          and regexp_contains(lower((select value.string_value 
+                                                     from unnest(event_params) 
+                                                     where key = 'action_label')), 'request_special')
+                     then concat(user_pseudo_id, "-", cast(event_timestamp as string)) 
+                end) as nb_requests_special
 
 from 
-  `project_id.analytics_web.events_*` as events,
+  `project_anon.analytics_web.events_*` as events,
   date_range
 
 where
   _table_suffix between date_range.start_date and date_range.end_date
 
-group by 1, 2, 4
+group by 1, 2, 3
 
