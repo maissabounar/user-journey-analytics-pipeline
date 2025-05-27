cc2d95af2239d6efbd0dee507b72b890deb984fa
Maïssa <104407324+maissabounar@users.noreply.github.com>
2025-05-27 15:10:26 +0200

    Update int_ref_companies.sql

--- models/intermediate/int_ref_companies.sql
+++ models/intermediate/int_ref_companies.sql
@@ -1,1 +1,7 @@
+select
+  cast(user_ref_id as string) as user_ref_id,
+  cast(company_id as string) as company_id,
+  coalesce(company_name, 'unknown') as company_name
+
+from `project_anon.external_ref.user_company_mapping`
 
